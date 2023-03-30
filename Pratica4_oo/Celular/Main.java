1d70f748c153032d6460e15376978288dccd9626
Nicolas Chi <94817330+Nkchina@users.noreply.github.com>
2023-03-30 03:16:08 -0300

    Update Main.java

--- Pratica4_oo/Celular/Main.java
+++ Pratica4_oo/Celular/Main.java
@@ -6,7 +6,7 @@ public class Main {
         CelularFabricanteA celularA = new CelularFabricanteA(true, true, true, false, true, true, 6800.00);
         CelularFabricanteB celularB = new CelularFabricanteB(true, true, true, true, false, true, 5200.00);
 
-        JOptionPane.showMessageDialog(null, "Celular Fabricante A: \n" +
+        JOptionPane.showMessageDialog(null, "Celular Fabricante A: \n\n" +
                 "Enviar mensagem: " + celularA.temEnviarMensagem() + "\n" +
                 "Acesso à internet: " + celularA.temAcessarInternet() + "\n" +
                 "E-mails: " + celularA.temAcessarEmails() + "\n" +
@@ -15,7 +15,7 @@ public class Main {
                 "Sistema de arquivos: " + celularA.temVerificarSistemaArquivos() + "\n" +
                 "Preço: R$ " + celularA.getPreco());
 
-        JOptionPane.showMessageDialog(null, "Celular Fabricante B: \n" +
+        JOptionPane.showMessageDialog(null, "Celular Fabricante B: \n\n" +
                 "Enviar mensagem: " + celularB.temEnviarMensagem() + "\n" +
                 "Acesso à internet: " + celularB.temAcessarInternet() + "\n" +
                 "E-mails: " + celularB.temAcessarEmails() + "\n" +
